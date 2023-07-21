76791d4bec88458fd203b744e871b08d2292132a
Misuiro <113217149+MizuruAndFelix@users.noreply.github.com>
2023-07-21 17:40:25 +0300

    1

---
+++ FibonachiAgain/FibonachiAgain.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int Fib(int num)
+{
+    if (num <= 1)
+    {
+        return num;
+    }
+    return Fib(num - 1) + Fib(num - 2);
+}
+
+int main()
+{
+    int num = 0;
+    cout << "Enter num: ";
+    cin >> num;
+    cout << endl << "Fibonachi nums:\n";
+    cout << Fib(num) << std::endl;
+}
